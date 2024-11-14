9b3ef737b41b8a342c3470506c025044134a40fd
Christian Fuentes <cfuentes@outlook.com>
2024-11-14 19:44:38 -0300

    Validaciones

--- transporteescolar/furgones/models.py
+++ transporteescolar/furgones/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.validators import RegexValidator
 from establecimientos.models import Establecimiento
+from django.core.exceptions import ValidationError
 from choferes.models import Chofer
 import re
 
@@ -19,6 +20,11 @@ class Furgon(models.Model):
                 params={'value': value},
             )
 
+    def validate_establecimientos(value):
+        if value.count() > 3:
+            raise ValidationError(
+                'Un furgón no puede estar asociado a más de 3 establecimientos.')
+
     patente = models.CharField(
         max_length=6, unique=True, validators=[validate_patente])
     marca = models.CharField(max_length=50)
@@ -29,7 +35,14 @@ class Furgon(models.Model):
         max_length=1, choices=ESTADO_CHOICES, default='A')
     chofer_asignado = models.ForeignKey(
         Chofer, on_delete=models.SET_NULL, null=True, blank=True)
-    establecimientos = models.ManyToManyField(Establecimiento, blank=True)
+    establecimientos = models.ManyToManyField(
+        Establecimiento, blank=True)
+
+    def clean(self):
+        super().clean()
+        if self.establecimientos.count() > 3:
+            raise ValidationError(
+                'Un furgón no puede estar asociado a más de 3 establecimientos.')
 
     def __str__(self):
         return self.patente
